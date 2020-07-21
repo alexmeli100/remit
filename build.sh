8c7e6d14f1e85a9154d8e1d86cd65032f5df9c2a
alexmeli100 <alexmeli100@gmail.com>
2020-07-21 19:48:10 -0400

    Added built script for circleci

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eo pipefail
+
+Services=('gateway' 'users' 'notificator')
+IMAGE_PREFIX='alexmeli'
+ROOT_DIR="$(pwd)"
+TAG='0.0'
+
+docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
+
+for svc in "${Services[@]}"; do
+  cd "${ROOT_DIR}$svc"
+
+  if [[ ! -f Dockerfile ]]; then
+    continue
+  fi
+
+  UNTAGGED_IMAGE="${IMAGE_PREFIX}/wealow-${svc}"
+  IMAGE="${UNTAGGED_IMAGE}:${TAG}"
+
+  echo "image: $IMAGE"
+  docker build -t "$IMAGE" .
+  docker push "${IMAGE}"
+done
