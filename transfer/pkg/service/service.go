d057a8b518bc03300629aef88d799fcbc13e9e78
alexmeli100 <alexmeli100@gmail.com>
2020-07-21 20:40:48 -0400

    added transfer service

--- transfer/pkg/service/service.go
+++ transfer/pkg/service/service.go
@@ -1,7 +1,41 @@
 package service
 
+import "context"
+
+const (
+	// list of operators
+	MTN    = "mtn"
+	Orange = "orange"
+)
+
+type SendOptions struct {
+	operator  string
+	recipient string
+	amount    string
+}
+
 // TransferService describes the service.
 type TransferService interface {
-	// Add your methods here
-	// e.x: Foo(ctx context.Context,s string)(rs string, err error)
+	Send(ctx context.Context, req *SendOptions) error
+}
+
+type basicTransferService struct{}
+
+func (b *basicTransferService) Send(ctx context.Context, req *SendOptions) (e0 error) {
+	// TODO implement the business logic of Send
+	return e0
+}
+
+// NewBasicTransferService returns a naive, stateless implementation of TransferService.
+func NewBasicTransferService() TransferService {
+	return &basicTransferService{}
+}
+
+// New returns a TransferService with all of the expected middleware wired in.
+func New(middleware []Middleware) TransferService {
+	var svc TransferService = NewBasicTransferService()
+	for _, m := range middleware {
+		svc = m(svc)
+	}
+	return svc
 }
