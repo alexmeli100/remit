a8becea5572f618d4c01483cef5c35c46cd031e3
alexmeli100 <alexmeli100@gmail.com>
2020-08-03 23:17:07 -0400

    switched to nats streaming for events

--- events/events.go
+++ events/events.go
@@ -3,19 +3,16 @@ package events
 import (
 	"context"
 	"github.com/alexmeli100/remit/users/pkg/grpc/pb"
+	"github.com/golang/protobuf/proto"
 	"github.com/nats-io/nats.go"
+	"github.com/nats-io/stan.go"
+	"github.com/pkg/errors"
 )
 
-type EventType int
 type UserEventHandler func(ctx context.Context, user *pb.User) error
 
 const (
 	UserEvents = "user-events"
-)
-
-const (
-	UserCreated       = "UserCreated"
-	UserPasswordReset = "UserPasswordReset"
 )
 
 type UserEvent struct {
@@ -24,42 +21,86 @@ type UserEvent struct {
 }
 
 type EventSender struct {
-	nats *nats.EncodedConn
+	nats stan.Conn
 }
 
 func (e *EventSender) OnUserCreated(ctx context.Context, u *pb.User) error {
-	return e.nats.Publish("user-events", UserEvent{UserCreated, u})
+	b, err := encodeUserEvent(pb.UserCreated, u)
+
+	if err != nil {
+		return errors.Wrap(err, "proto marshal error")
+	}
+
+	return e.nats.Publish("user-events", b)
 }
 
 func (e *EventSender) OnPasswordReset(ctx context.Context, u *pb.User) error {
-	return e.nats.Publish("user-events", UserEvent{UserPasswordReset, u})
+	b, err := encodeUserEvent(pb.UserPasswordReset, u)
+
+	if err != nil {
+		return errors.Wrap(err, "proto marshal error")
+	}
+
+	return e.nats.Publish("user-events", b)
 }
 
-func NewEventSender(conn *nats.EncodedConn) EventManager {
+func encodeUserEvent(kind pb.UserEventKind, u *pb.User) ([]byte, error) {
+	event := &pb.UserEvent{
+		Kind: kind,
+		User: u,
+	}
+
+	return proto.Marshal(event)
+}
+
+func decodeUserEvent(data []byte) (*pb.UserEvent, error) {
+	event := pb.UserEvent{}
+
+	if err := proto.Unmarshal(data, &event); err != nil {
+		return nil, errors.Wrap(err, "proto unmarshall error")
+	}
+
+	return &event, nil
+}
+
+func NewEventSender(conn stan.Conn) EventManager {
 	return &EventSender{conn}
 }
 
-func Connect(url string) (*nats.EncodedConn, error) {
-	conn, err := nats.Connect(url)
+func Connect(url, clusterID, clientID string) (stan.Conn, error) {
+	nc, err := nats.Connect(url)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return nats.NewEncodedConn(conn, nats.JSON_ENCODER)
+	return stan.Connect(clusterID, clientID, stan.NatsConn(nc))
 }
 
-func ListenUserEvents(ctx context.Context, conn *nats.EncodedConn, queue string, handlers map[string]UserEventHandler) (chan error, error) {
+func ListenUserEvents(ctx context.Context, conn stan.Conn, queue string, handlers map[pb.UserEventKind]UserEventHandler, opts ...stan.SubscriptionOption) (chan error, error) {
 	errc := make(chan error, 10)
 
-	subs, err := conn.QueueSubscribe(UserEvents, queue, func(e *UserEvent) {
-		var err error
-		err = handlers[e.EventKind](ctx, e.User)
+	handler := func(msg *stan.Msg) {
+		e, err := decodeUserEvent(msg.Data)
 
 		if err != nil {
 			errc <- err
+			return
 		}
-	})
+
+		err = handlers[e.Kind](ctx, e.User)
+
+		if err != nil {
+			errc <- err
+			return
+		}
+
+		if err = msg.Ack(); err != nil {
+			errc <- err
+		}
+	}
+
+	subs, err := conn.QueueSubscribe(UserEvents, queue, handler, opts...)
 
 	if err != nil {
 		return nil, err
@@ -73,13 +114,13 @@ func ListenUserEvents(ctx context.Context, conn *nats.EncodedConn, queue string,
 	return errc, nil
 }
 
-func ListenAllUserEvents(ctx context.Context, conn *nats.EncodedConn, queue string, sink UserEventManager) (chan error, error) {
-	handlers := map[string]UserEventHandler{
-		UserCreated:       sink.OnUserCreated,
-		UserPasswordReset: sink.OnPasswordReset,
+func ListenAllUserEvents(ctx context.Context, conn stan.Conn, queue string, sink UserEventManager, opts ...stan.SubscriptionOption) (chan error, error) {
+	handlers := map[pb.UserEventKind]UserEventHandler{
+		pb.UserCreated:       sink.OnUserCreated,
+		pb.UserPasswordReset: sink.OnPasswordReset,
 	}
 
-	errc, err := ListenUserEvents(ctx, conn, queue, handlers)
+	errc, err := ListenUserEvents(ctx, conn, queue, handlers, opts...)
 
 	if err != nil {
 		return nil, err
--- events/go.mod
+++ events/go.mod
@@ -3,6 +3,8 @@ module github.com/alexmeli100/remit/events
 go 1.14
 
 require (
-	github.com/alexmeli100/remit/users v0.0.0-20200802163037-9e74b55a7a5c
+	github.com/alexmeli100/remit/users v0.0.0-20200804011556-5848f317360d
+	github.com/nats-io/nats-streaming-server v0.18.0 // indirect
 	github.com/nats-io/nats.go v1.10.0
+	github.com/nats-io/stan.go v0.7.0
 )
