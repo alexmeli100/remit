79f8f654a5c40714c0567b91a460ba28dc4b8320
alexmeli100 <alexmeli100@gmail.com>
2020-08-11 15:46:56 -0400

    created transfer module

---
+++ payment/pkg/service/service.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	"context"
+	"github.com/alexmeli100/remit/payment/pkg/grpc/pb"
+)
+
+// PaymentService describes the service.
+type PaymentService interface {
+	SaveCard(ctx context.Context, uid string) (string, error)
+	GetPaymentIntentSecret(ctx context.Context, req *pb.PaymentRequest) (string, error)
+	CapturePayment(ctx context.Context, pi string, amount float64) (string, error)
+}
+
+// New returns a PaymentService with all of the expected middleware wired in.
+func New(svc PaymentService, middleware []Middleware) PaymentService {
+	for _, m := range middleware {
+		svc = m(svc)
+	}
+
+	return svc
+}
